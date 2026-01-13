cfd64992e14db8ab3f32df75c9bf50c0463af193
Diogo <diogoangelim@gmail.com>
2026-01-13 18:24:00 -0300

    add status badges

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  test: {
+    include: [
+      '**/*.{test,spec}.{ts,tsx}'
+    ],
+    exclude: [
+      '**/node_modules/**',
+      '**/.git/**'
+    ],
+    environment: 'jsdom',
+  },
   coverage: {
     provider: 'v8',
     reporter: ['lcov', 'text'],
